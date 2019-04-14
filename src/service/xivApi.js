bd46b816f580983e218662b3b39b46df230be748
Stacy <stacy@Stacys-MacBook-Pro.local>
2019-04-14 14:22:28 -0400

    add button to try import again when freshly bringing in data

--- src/service/xivApi.js
+++ src/service/xivApi.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
-import { get as _get } from 'lodash'
+import { get as _get, set as _set } from 'lodash'
+// import UNLOADED_CHARACTER from '../data/mock/unloadedCharacter'
 
 // const PRIVATE_KEY = '257f7d4532a74f15a429b5262d51d0f3938964ea78124b1ca8da9459accc15b7'
 const BASE_URL = 'https://xivapi.com'
@@ -11,6 +12,7 @@ export const searchForCharacter = async (server, name, page = 1) => {
 
 export const getCharacter = async id => {
   const result = await axios.get(`${BASE_URL}/character/${id}?extended=1`)
+  _set(result, 'data.characterId', id)
   return _get(result, 'data', {})
 }
 
