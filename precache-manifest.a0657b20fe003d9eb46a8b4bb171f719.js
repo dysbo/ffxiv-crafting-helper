285881c4543d75847743b5a45dba8bb811bbc1c2
slw5011 <slw5011@gmail.com>
2019-05-20 20:34:53 -0400

    Updates

--- precache-manifest.a0657b20fe003d9eb46a8b4bb171f719.js
+++ precache-manifest.a0657b20fe003d9eb46a8b4bb171f719.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/ffxiv-crafting-helper/static/js/runtime~main.7eeda2ce.js"
   },
   {
-    "revision": "1c1ae8d221da6d6f631d",
-    "url": "/ffxiv-crafting-helper/static/js/main.483bf210.chunk.js"
+    "revision": "1c96cdc58f5ebeee2916",
+    "url": "/ffxiv-crafting-helper/static/js/main.c07148d4.chunk.js"
   },
   {
     "revision": "d92f4aeeaf76266fd774",
     "url": "/ffxiv-crafting-helper/static/js/2.7e97b049.chunk.js"
   },
   {
-    "revision": "1c1ae8d221da6d6f631d",
+    "revision": "1c96cdc58f5ebeee2916",
     "url": "/ffxiv-crafting-helper/static/css/main.f46bbf13.chunk.css"
   },
   {
-    "revision": "a0e8b05c7cfad852ebe400f704089754",
+    "revision": "6b103245817b12037770c413c7dbf514",
     "url": "/ffxiv-crafting-helper/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ffxiv-crafting-helper/precache-manifest.2dd9005cafb11a2d2a7c447f4b46b876.js"
+  "/ffxiv-crafting-helper/precache-manifest.a0657b20fe003d9eb46a8b4bb171f719.js"
 );
 
 workbox.clientsClaim();
