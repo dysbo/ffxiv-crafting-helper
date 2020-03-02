590713f939f8e02db6de0661a716e483b0f442a2
slw5011 <slw5011@gmail.com>
2020-03-01 21:32:08 -0500

    Updates

---
+++ precache-manifest.0863b87b78e42605d102ba24f35832b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2721202c91c63ed3f7abcf25c82aac45",
+    "url": "/ffxiv-crafting-helper/index.html"
+  },
+  {
+    "revision": "3fa6c0930f759697421c",
+    "url": "/ffxiv-crafting-helper/static/css/main.a9b3a1b6.chunk.css"
+  },
+  {
+    "revision": "87d491fcf835ffb26b93",
+    "url": "/ffxiv-crafting-helper/static/js/2.62c9d7d8.chunk.js"
+  },
+  {
+    "revision": "8ed227bf84a90762c098dc3eb6e8cfef",
+    "url": "/ffxiv-crafting-helper/static/js/2.62c9d7d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fa6c0930f759697421c",
+    "url": "/ffxiv-crafting-helper/static/js/main.075b090b.chunk.js"
+  },
+  {
+    "revision": "10c6e0314697b1b1e173",
+    "url": "/ffxiv-crafting-helper/static/js/runtime-main.24ccf699.js"
+  }
+]);
